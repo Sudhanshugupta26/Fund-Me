58c0463e785317a002973396973f78600e14f938
SudhanshuGupta26 <guptasudhanshukumar1@gmail.com>
2025-12-19 10:15:02 +0000

    HelperConfig.sol Done

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -3,15 +3,20 @@ pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        // Before anything startbroadcast() => Not a Tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         // Only broadcast if running in actual script mode (not in tests)
         if (msg.sender != address(this)) {
             vm.startBroadcast();
         }
 
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
 
         if (msg.sender != address(this)) {
             vm.stopBroadcast();
