f98442838bddf1d462703823061caaa1f1be04bc
SudhanshuGupta26 <guptasudhanshukumar1@gmail.com>
2025-12-19 16:11:01 +0000

    Test optimization

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -27,9 +27,9 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        console.log("Owner Address:", fundme.i_owner());
+        console.log("Owner Address:", fundme.getOwner());
         console.log("Msg Sender Address:", msg.sender);
-        assertEq(fundme.i_owner(), msg.sender);
+        assertEq(fundme.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionisAccurate() public {
@@ -49,5 +49,73 @@ contract FundMeTest is Test {
 
         uint256 amountFunded = fundme.getAddressToAmountFunded(USER);
         assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public {
+        vm.prank(USER);
+        fundme.fund{value: SEND_VALUE}();
+        address funder = fundme.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundme.fund{value: SEND_VALUE}();
+        _;
+    }
+
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER); // USER1 that sends the value.
+        fundme.fund{value: SEND_VALUE}();
+
+        vm.prank(USER); //USER2 which is not the owner(USER1).
+        vm.expectRevert(); //since USER2 != USER1
+        fundme.withdraw(); //this should revert
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // the user is already created and funded
+        // Arrange
+        uint256 startingOwnerBalance = fundme.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundme).balance;
+
+        // Act
+        vm.prank(fundme.getOwner());
+        fundme.withdraw();
+
+        // Assert
+        uint256 endingOwnerBalance = fundme.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundme).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+        // should be 0
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        //Arrange
+        uint160 numberOfFunders = 10;
+        uint160 startingNumber = 1;
+        for (uint160 i = startingNumber; i < numberOfFunders; i++) {
+            hoax(address(i), SEND_VALUE);
+            fundme.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundme.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundme).balance;
+
+        //Act
+        vm.startPrank(fundme.getOwner());
+        fundme.withdraw();
+        vm.stopPrank();
+
+        //assert
+        assert(address(fundme).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundme.getOwner().balance
+        );
     }
 }
